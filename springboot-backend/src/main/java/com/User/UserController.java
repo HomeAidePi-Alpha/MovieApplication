228445db66ee5cd009889a2af2ec7fc4177bc929
clh7090 <clh7090@rit.edu>
2021-12-25 00:12:05 -0500

    Added User Functionality.

--- springboot-backend/src/main/java/com/User/UserController.java
+++ springboot-backend/src/main/java/com/User/UserController.java
@@ -1,4 +1,52 @@
 package com.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping("/users")
+    public List<UserModel> getAllUsers() {
+        return userService.getAllUsers(); // returns a list of users
+    }
+
+
+    @PostMapping("/users")
+    public UserModel createUser(@RequestBody UserModel user) {
+        return userService.createUser(user);
+    }
+
+
+    @GetMapping("/users/{username}")
+    public ResponseEntity<UserModel> getUserById(@PathVariable String username) {
+        UserModel user = userService.getEmployeeById(username);
+        return ResponseEntity.ok(user);
+    }
+
+
+    @PutMapping("/users/{username}")
+    public ResponseEntity<UserModel> updateUser(@PathVariable String username, @RequestBody UserModel userDetails) {
+        UserModel updatedUser = userService.updateUser(username, userDetails);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+
+    @DeleteMapping("/users/{username}")
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable String username) {
+        userService.deleteEmployee(username);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", true); // if the resource didn't exist it was still 'deleted'
+        return ResponseEntity.ok(response);
+    }
 }
--- springboot-backend/src/main/java/com/User/UserService.java
+++ springboot-backend/src/main/java/com/User/UserService.java
@@ -1,4 +1,48 @@
 package com.User;
 
+import com.Exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<UserModel> getAllUsers() {
+        return userRepository.findAll(); // returns a list of users
+    }
+
+
+    public UserModel createUser(UserModel user) {
+        return userRepository.save(user);
+    }
+
+
+    public UserModel getEmployeeById(String username) {
+        return userRepository.findById(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User With the username: " + username + " does not exist!"));
+    }
+
+
+    public UserModel updateUser(String username, UserModel userDetails) {
+        UserModel user = userRepository.findById(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User With the username: " + username + " does not exist!"));
+
+        user.setUsername(userDetails.getUsername());
+        user.setPassword(userDetails.getPassword());
+        user.setEmail(userDetails.getEmail());
+
+        return userRepository.save(user);
+    }
+
+
+    public void deleteEmployee(String username) {
+        UserModel user = userRepository.findById(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User With the username: " + username + " does not exist!"));
+        userRepository.delete(user);
+    }
 }
