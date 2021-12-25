9c51424c4c78e62615c6a16a185283d4cb2c20b5
clh7090 <clh7090@rit.edu>
2021-12-25 00:25:13 -0500

    Stub tests.

---
+++ springboot-backend/src/test/java/com/User/UserModelTest.java
@@ -0,0 +1,58 @@
+package com.User;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserModelTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getUsername() {
+    }
+
+    @Test
+    void setUsername() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void getCreationDate() {
+    }
+
+    @Test
+    void setCreationDate() {
+    }
+
+    @Test
+    void getLastAccessDate() {
+    }
+
+    @Test
+    void setLastAccessDate() {
+    }
+}
